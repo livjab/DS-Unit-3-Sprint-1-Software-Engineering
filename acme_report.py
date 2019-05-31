6402a77f535d00fa71536b7cf619ed8191ba4c4a
Liv Johnson <livjab@gmail.com>
2019-05-31 18:42:19 -0500

    boom

--- acme_report.py
+++ acme_report.py
@@ -16,7 +16,7 @@ def generate_products(n=30):
         name = choice(ADJECTIVES) + " " + choice(NOUNS)
         price = randint(5, 101)
         weight = randint(5, 101)
-        flammability = [x / 10.0 for x in range(0, 25)]
+        flammability = uniform(0, 2.5)
 
         products.append(Product(name=name, price=price, weight=weight,
                                 flammability=flammability))
@@ -40,28 +40,24 @@ def generate_products(n=30):
 def inventory_report(products):
 
     unique_products = []
-    average_price = []
-    average_weight = []
-    average_flammability = []
+    average_price = 0
+    average_weight = 0
+    average_flammability = 0
 
     for product in products:
         unique_products.append(len(set(products)))
-
-    #for product in products:
-    #    average_price.append(sum(product.price) / len(product.price))
-
-    #for product in products:
-    #    average_weight.append(sum(product.weight) / len(product.weight))
-
-    #for product in products:
-    #    average_flammability.append(sum(product.flammability) / len(product.flammability))
-
+        average_price += product.price
+        average_weight += product.weight
+        average_flammability += product.flammability
+    average_price = average_price / 30
+    average_weight = average_weight / 30
+    average_flammability = average_flammability / 30
 
     print("ACME CORPORATION OFFICIAL INVENTORY REPORT")
     print(f'Number of Unique Products: {unique_products[0]}')
-    #print(f'Average Price: {average_price[0]}')
-    #print(f'Average Weight: {average_weight[0]}')
-    #print(f'Average Flammability: {average_flammability[0]}')
+    print(f'Average Price: {average_price}')
+    print(f'Average Weight: {average_weight}')
+    print(f'Average Flammability: {average_flammability}')
 
 if __name__ == '__main__':
     inventory_report(generate_products())
